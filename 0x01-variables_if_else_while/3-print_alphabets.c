a8cac43409f5b6ce1127a4791f40b9369ca3effb
Amimej1010 <anigbosonjemima@yahoo.com>
2023-06-19 12:28:48 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	putchar(lowercase);
+	for (lowercase = 'A'; lowercase <= 'Z'; lowercase++)
+	putchar(lowercase);
+	putchar('\n');
+	return (0);
+}
